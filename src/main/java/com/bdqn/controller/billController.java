c5b48ddf8055ad10ebabee44e8dcd597c843bfd6
wjt <1009609575@qq.com>
2019-09-16 14:53:26 +0800

    第一次提交  完成的是小比比功能

---
+++ src/main/java/com/bdqn/controller/billController.java
@@ -0,0 +1,15 @@
+package com.bdqn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *@ClassName:billController
+ *@Description:订单控制器
+ *@Author:lzq
+ *@Date: 2019/9/16 14:52
+ **/
+@Controller
+@RequestMapping("/bill")
+public class billController {
+}
